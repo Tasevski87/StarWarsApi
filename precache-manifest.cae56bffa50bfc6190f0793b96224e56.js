33ff013a6574e64f0741a6dd4595a97c5be187fe
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-07-01 10:10:43 -0400

    Updates

---
+++ precache-manifest.cae56bffa50bfc6190f0793b96224e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4631d65d56884b2887729566dce243",
+    "url": "/StarWarsApi/index.html"
+  },
+  {
+    "revision": "2504f7bee1b9e8251f4a",
+    "url": "/StarWarsApi/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "9be2568482fc1b1ae314",
+    "url": "/StarWarsApi/static/js/2.6426c65b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/StarWarsApi/static/js/2.6426c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2504f7bee1b9e8251f4a",
+    "url": "/StarWarsApi/static/js/main.f6f5d47d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/StarWarsApi/static/js/main.f6f5d47d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8709ab0cf8080b3eda69",
+    "url": "/StarWarsApi/static/js/runtime-main.2e9b1b52.js"
+  }
+]);
